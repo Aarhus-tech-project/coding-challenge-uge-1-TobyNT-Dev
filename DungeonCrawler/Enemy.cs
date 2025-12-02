d1ded15207f2ec73dcff4b040d81c5613413e2fa
Tobias Thy <t.nt.dev.code@gmail.com>
2025-12-02 22:06:40 +0100

    Revert "Merge branch 'feedback' of https://github.com/Aarhus-tech-project/coding-challenge-uge-1-TobyNT-Dev into feedback"
    This reverts commit 06b1730a627d0b0655ddc2191c0467114734356e, reversing
    changes made to d3976e7b49c7faec17c78bbed976ce222e58b3ce.

--- DungeonCrawler/Enemy.cs
+++ DungeonCrawler/Enemy.cs
@@ -1,11 +1,11 @@
-﻿namespace DungeonCrawler
+﻿using System.Linq;
+
+namespace DungeonCrawler
 {
     internal class Enemy
     {
         private Random rnd = new Random();
         MainGame main = new();
-        PlayerCharacter player = new();
-        AttackCalculation attackCalculation = new();
 
         char[] monsterTypes = { 'Z' };
 
@@ -22,31 +22,23 @@
             positionZ = _positionZ;
             monsterType = monsterTypes[rnd.Next(0, monsterTypes.Length)];
         }
-        public void TakeTurn(int playerX, int playerZ, List<Enemy> enemies)
+        public void TakeTurn(int playerX, int playerZ)
         {
             int newX = positionX;
             int newZ = positionZ;
 
-            // Coordinate distance between me (enemy) and player
-            int distX = playerX - positionX;
-            int distZ = playerZ - positionZ;
-
-            // absolute distance between me (enemy) and player
-            int absDistX = Math.Abs(distX);
-            int absDistZ = Math.Abs(distZ);
-
-            // direction player is in
-            int dirX = Math.Sign(distX);
-            int dirZ = Math.Sign(distZ);
-
-            
-
-            //if player is close enough to attack
-            if (absDistX <= 1 || absDistZ <= 1)
+            int directionX = playerX - positionX;
+            int directionZ = playerZ - positionZ;
+            //is player within detection range
+            if (Math.Abs(directionX) < detectionDistance || Math.Abs(directionZ) < detectionDistance)
+            {
+                MoveTowardPlayer(directionX, directionZ, newX, newZ);
+            }
+            //is player close enough to attack
+            else if (playerX == positionX + 1 || playerX == positionX - 1 || playerZ == positionZ + 1 || playerZ == positionZ - 1)
             {
                 AttackPlayer();
             }
-            //if not, do noting
             else
             {
                 return;
@@ -55,9 +47,55 @@
 
         private void AttackPlayer()
         {
-            int attackDamage = attackCalculation.CalculateAttack("player");
+            //roll attack damage
+            int rndNum = rnd.Next(0, 101);
+            int attackDamage;
+
+            switch (rndNum)
+            {
+                // 0–49  (50% chance) Attack missed
+                case < 50:
+                    attackDamage = 0; // 0 dmg
+                    break;
+                // 50–74 (25% chance) normal attack hit
+                case < 75:
+                    attackDamage = rnd.Next(10, 16); // 10–15 dmg
+                    break;
+                // 75–89 (15% chance) strong attack hit
+                case < 90:
+                    attackDamage = rnd.Next(15, 21); // 15–20 dmg
+                    break;
+                // 90–98 (9% chance) very strong attack hit
+                case < 99:
+                    attackDamage = rnd.Next(20, 31); // 20–30 dmg
+                    break;
+                // 100 (1% chance) critical attack hit
+                case 100:
+                    attackDamage = rnd.Next(50, 101); // 50–100 dmg
+                    break;
+            }
             //attack player
-            player.TakeDamage(attackDamage);
+        }
+
+        private void MoveTowardPlayer(int directionX, int directionZ, int newX, int newZ)
+        {
+            if (rnd.Next(0, 2) == 0)
+            {
+                newX += Math.Sign(directionX);
+            }
+            else
+            {
+                newZ += Math.Sign(directionZ);
+            }
+
+            Console.SetCursorPosition(positionX, positionZ);
+            Console.Write(main.floor[0]);
+
+            Console.SetCursorPosition(newX, newZ);
+            Console.Write(monsterType.ToString());
+
+            positionX = newX;
+            positionZ = newZ;
         }
     }
 }
